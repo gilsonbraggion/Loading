8049d6d338c0faa2e0fb2096a7c1b6ebe085fe5e
Gilson Braggion <gilsonbraggion@MacBook-Pro-de-Gilson.local>
2017-11-26 23:27:58 -0200

    inclusao do projeto

---
+++ src/main/java/br/com/rhinosistemas/LoadingApplication.java
@@ -0,0 +1,12 @@
+package br.com.rhinosistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadingApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	$(".show-loader").click(function() {
+		$("#myModal").modal("show");
+	});
+	
+	
+	$("#loader").click(function() {
+		debugger;
+		$("#myModal").modal("hide");
+	});
+});
